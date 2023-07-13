06e547cb9193f5a9faef9fae21d75ed1531c93dc
JuanDavidGanan <138254966+JuanDavidGanan@users.noreply.github.com>
2023-07-13 08:27:04 -0500

    Update db.js

--- servidor/database/db.js
+++ servidor/database/db.js
@@ -1,8 +1,8 @@
 import { Sequelize } from 'sequelize';
 
 //Se conecta a la base de datos usando sequlize, con los parametros de esta misma "nombre base de datos" "usuario" "constrasena"
-const db = new Sequelize('tienda', 'root', 'root', {
-    host:'localhost',
+const db = new Sequelize('bw9yglakrjdohml4h83y', 'urbgc7jw8qbh9ycn', 'urbgc7jw8qbh9ycn', {
+    host:'bw9yglakrjdohml4h83y-mysql.services.clever-cloud.com',
     dialect:'mysql'
 });
 export default db;
