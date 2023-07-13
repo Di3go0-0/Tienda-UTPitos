8b81fa4aa702f97ee7558f9f2999bcad1fe07496
Di3go0-0 <d.rincon@utp.edu.co>
2023-07-13 14:00:11 -0500

    safe database

--- servidor/database/db.js
+++ servidor/database/db.js
@@ -1,8 +1,8 @@
 import { Sequelize } from 'sequelize';
 
 //Se conecta a la base de datos usando sequlize, con los parametros de esta misma "nombre base de datos" "usuario" "constrasena"
-const db = new Sequelize('bw9yglakrjdohml4h83y', 'urbgc7jw8qbh9ycn', 'urbgc7jw8qbh9ycn', {
-    host:'bw9yglakrjdohml4h83y-mysql.services.clever-cloud.com',
+const db = new Sequelize('bysnmbtrbyjbcaad1hqq', 'uhzngho6brgx4eto', 'N67KdhXVETNQmJMzbHSc', {
+    host : 'bysnmbtrbyjbcaad1hqq-mysql.services.clever-cloud.com',
     dialect:'mysql'
 });
 export default db;
